c5444c41b25758d470e0f656e0944f4debf2fbed
think-gold <118820315+think-gold@users.noreply.github.com>
2023-04-27 19:44:52 +0200

    task1 solved

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,59 +8,61 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        takeOperation();
+        calculator();
 
         /* Kalkulator: napisz program kalkulator, który wspiera działania dodawania, odejmowania, mnożenia i dzielenia. Użytkownik wpisuje działanie, np 4 + 5 i otrzymuje wynik: 4 + 5 = 9.*/
 
-
     }
 
-//Input
-/*public static String takeName(){
-        System.out.println("Give me your name: " );
-        Scanner Scanner = new Scanner(System.in);
-        String yourName = Scanner.nextLine();
-        return yourName;
-    }*/
-
-    // Dekompozycja problemu:
-    // 1. Wyświetlić prośbę o podanie działania
-    // 2. Odczytać działanie
-    // 3. Wyświetlić prośbę o podanie liczb
-    // 4. Wykonać działanie (jeśli nie wybrano dzielenia, a druga z liczb nie jest 0)
-    // 5. Wyświetlić wynik lub komunikat, że nie jest to możliwejud
-
-    //give null
+    public static void calculator() {
+        String [] operation = takeOperation();
+        Integer numberA = getNumber(operation, 0);
+        Integer numberB = getNumber(operation, 2);
+        String operator = operator(operation);
+        while (operator.equals("/") && numberB == 0) {
+            divisionError();  // dodać metodę pobierającą nową wartość dla B
+        }
+        float result = calculation(operator, numberA, numberB);
+        displayResult(numberA, numberB, operator, result);
+    }
 
     public static String[] takeOperation() {
         System.out.println("Give operation and numbers, ex. 4 + 5: ");
         Scanner Scanner = new Scanner(System.in);
         String operation = Scanner.nextLine();
-
         String[] operationArray = operation.split(" ");
-        for (String word : operationArray) {
-            System.out.println(word);
-        }
         return operationArray;
     }
 
-    public static Integer numberA() {
-        String[] charA = takeOperation();
-        String characterA = charA[0];
-        Integer numberA = Integer.parseInt(characterA);
-        return numberA;
+    public static Integer getNumber(String[] operation, Integer i) {
+        String characterA = operation[i];
+        Integer number = Integer.parseInt(characterA);
+        return number;
     }
-    public static String operator() {
-        String[] charOp = takeOperation();
-        String operator = charOp[1];
+    public static String operator(String[] operation) {
+
+        String operator = operation[1];
         return operator;
     }
-    public static Integer numberB() {
-        String[] charB = takeOperation();
-        String characterB = charB[2];
-        Integer numberB = Integer.parseInt(characterB);
-        return numberB;
+
+    public static void divisionError() {   // w jaki sposob mozemy wplywac na parametry przeslane do metody?
+        System.out.println("Nie można dzielić przez 0!!!");
+        System.out.println("Wprowadź liczbę różną od zera");
     }
-    
+    public static float calculation(String operator, Integer a, Integer b) {
+        if (operator.equals("+")) {
+            return a + b;
+        } else if (operator.equals("-")) {
+            return a - b;
+        } else if (operator.equals("*")) {
+            return a * b;
+        } else {
+            return a / b;
+        }
+    }
+    public static void displayResult(Integer a, Integer b, String operator, Float result) {
+        String messageFormatted = String.format("%d %s %d = %f", a, operator, b, result);
+        System.out.println(messageFormatted);
+    }
 }
 
