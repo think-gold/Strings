23314db622644a1b4c7764eaee0fffca7bfca169
think-gold <118820315+think-gold@users.noreply.github.com>
2023-05-01 11:08:22 +0200

    added new function in divisionError (calculator)
    collectiong new number when user tries to dived by 0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,20 +8,19 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        takeText();
-      //  calculator();
-
-        /* Kalkulator: napisz program kalkulator, który wspiera działania dodawania, odejmowania, mnożenia i dzielenia. Użytkownik wpisuje działanie, np 4 + 5 i otrzymuje wynik: 4 + 5 = 9.*/
-
+        /* ZAD 1. Kalkulator: napisz program kalkulator, który wspiera działania dodawania, odejmowania, mnożenia i dzielenia.
+        Użytkownik wpisuje działanie, np 4 + 5 i otrzymuje wynik: 4 + 5 = 9.*/
+        calculator();
+        
     }
 
     public static void calculator() {
-        String [] operation = takeOperation();
+        String[] operation = takeOperation();
         Integer numberA = getNumber(operation, 0);
         Integer numberB = getNumber(operation, 2);
         String operator = operator(operation);
         while (operator.equals("/") && numberB == 0) {
-            divisionError();  // dodać metodę pobierającą nową wartość dla B
+            numberB = divisionError();
         }
         float result = calculation(operator, numberA, numberB);
         displayResult(numberA, numberB, operator, result);
@@ -29,8 +28,8 @@ public class App {
 
     public static String[] takeOperation() {
         System.out.println("Give operation and numbers, ex. 4 + 5: ");
-        Scanner Scanner = new Scanner(System.in);
-        String operation = Scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
         String[] operationArray = operation.split(" ");
         return operationArray;
     }
@@ -40,16 +39,21 @@ public class App {
         Integer number = Integer.parseInt(characterA);
         return number;
     }
+
     public static String operator(String[] operation) {
 
         String operator = operation[1];
         return operator;
     }
 
-    public static void divisionError() {   // w jaki sposob mozemy wplywac na parametry przeslane do metody?
+    public static Integer divisionError() {   // w jaki sposob mozemy wplywac na parametry przeslane do metody?
         System.out.println("Nie można dzielić przez 0!!!");
-        System.out.println("Wprowadź liczbę różną od zera");
+        System.out.print("Wprowadź liczbę różną od zera: ");
+        Scanner scanner = new Scanner(System.in);
+        Integer newNumber = scanner.nextInt();
+        return newNumber;
     }
+
     public static float calculation(String operator, Integer a, Integer b) {
         if (operator.equals("+")) {
             return a + b;
@@ -61,28 +65,27 @@ public class App {
             return a / b;
         }
     }
+
     public static void displayResult(Integer a, Integer b, String operator, Float result) {
         String messageFormatted = String.format("%d %s %d = %f", a, operator, b, result);
         System.out.println(messageFormatted);
-
-
     }
-    //zad.2
-    public static void takeText() {
-        System.out.println("Give text: ");
-        Scanner Scanner = new Scanner(System.in);
-        String operation = Scanner.nextLine();
-
-        for (Integer i = 0; i < operation.length(); i++) {
-            Character character = operation.charAt(i);
-          //  if(operation.charAt(i).equals(" ") && operation.charAt(i+1)==" ");
-            System.out.println(character);
-        }
-        //return operationArray;
-        //for (String singleText : operationArray) {
-           // String singleText1=singleText.trim();
-           // System.out.println(singleText);
-       // }
-    }
+//    //zad.2
+//    public static void takeText() {
+//        System.out.println("Give text: ");
+//        Scanner Scanner = new Scanner(System.in);
+//        String operation = Scanner.nextLine();
+//
+//        for (Integer i = 0; i < operation.length(); i++) {
+//            Character character = operation.charAt(i);
+//          //  if(operation.charAt(i).equals(" ") && operation.charAt(i+1)==" ");
+//            System.out.println(character);
+//        }
+//        //return operationArray;
+//        //for (String singleText : operationArray) {
+//           // String singleText1=singleText.trim();
+//           // System.out.println(singleText);
+//       // }
 }
+
 
